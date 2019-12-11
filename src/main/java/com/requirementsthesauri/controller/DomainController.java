860d41d260c861504e581fd86f25bca1d2900d44
rozacarias <rodrigozacarias@id.uff.br>
2019-12-11 16:45:01 -0300

    Começando o método get domain

--- src/main/java/com/requirementsthesauri/controller/DomainController.java
+++ src/main/java/com/requirementsthesauri/controller/DomainController.java
@@ -3,9 +3,11 @@ package com.requirementsthesauri.controller;
 import com.requirementsthesauri.model.Domain;
 import com.requirementsthesauri.service.DomainService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +17,30 @@ public class DomainController{
 
     private DomainService domainService = new DomainService();
 
-    @GetMapping
+    @GetMapping("/getAllDomains")
     public ResponseEntity<?> getAllDomains(){
         return new ResponseEntity<>(domainService.getAllDomains(), HttpStatus.OK);
     }
 
-    @PostMapping(path = "/createDomainsList")
+    @GetMapping(value = "/{domainID}", produces = {"application/json",
+            "application/ld+json",
+            "application/n-triples",
+            "application/rdf+xml",
+            "application/turtle",
+            "application/rdf+json"})
+    public ResponseEntity<?> getDomain(@PathParam("domainID") String domainID, @RequestHeader("Accept") String accept){
+        return new ResponseEntity<>(domainService.getAllDomains(), HttpStatus.OK);
+    }
+
+
+    @PostMapping(path = "/createDomainsList", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createDomainList(@RequestBody List<Domain> domains){
         return new ResponseEntity<>(domainService.createDomain(domains), HttpStatus.CREATED);
     }
 
-    @PostMapping(path = "/createDomain")
+    @PostMapping(path = "/createDomain", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createDomain(@RequestBody Domain domain){
         List<Domain> domains  = new ArrayList<>();
         domains.add(domain);
--- src/main/java/com/requirementsthesauri/service/DomainService.java
+++ src/main/java/com/requirementsthesauri/service/DomainService.java
@@ -2,15 +2,16 @@ package com.requirementsthesauri.service;
 
 import com.requirementsthesauri.model.Domain;
 import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.update.UpdateRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonWriter;
+import javax.json.*;
+import javax.xml.ws.Response;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
@@ -135,6 +136,125 @@ public class DomainService {
         return querySelect;
     }
 
+    public ResponseEntity<?> getDomain(String domainID, String accept) {
+        authentication.getAuthentication();
+
+        String queryDescribe = getCompanySparqlDescribe(domainID);
+
+        Query query = QueryFactory.create(queryDescribe);
+        QueryExecution qx = QueryExecutionFactory.sparqlService(sparqlEndpoint, query);
+
+        Model rst = qx.execDescribe();
+        if (rst.isEmpty()) {
+            return ResponseEntity.status(422).build();
+        }
+
+        if(accept.equals("application/json") || isValidFormat(accept)==false) {
+
+            String querySelect = getCompanySparqlSelect(domainID);
+            Query queryS = QueryFactory.create(querySelect);
+            QueryExecution qexec = QueryExecutionFactory.sparqlService(sparqlEndpoint, queryS);
+            ResultSet results = qexec.execSelect();
+
+            QuerySolution soln = results.nextSolution();
+            String companyURL = soln.getResource("companyURL").toString() ;
+            String legalName = soln.getLiteral("legalName").toString() ;
+            String email = soln.getResource("email").toString() ;
+            String catalogURI = soln.getResource("catalogURI").toString() ;
+
+            JsonObject jobj = Json.createObjectBuilder()
+                    .add("id", domainID)
+                    .add("companyURL",companyURL)
+                    .add("legalName",legalName)
+                    .add("email",email)
+                    .add("catalogURI",catalogURI)
+                    .build();
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            JsonWriter writer = Json.createWriter(outputStream);
+            writer.writeObject(jobj);
+            String output = new String(outputStream.toByteArray());
+            writer.close();
+
+            return ResponseEntity.status(422).build();
+
+        }else {
+
+            String format = convertFromAcceptToFormat(accept);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            rst.write(outputStream, format);
+            String output = new String(outputStream.toByteArray());
+
+            return ResponseEntity.status(422).build();
+        }
+    }
+
+    public String convertFromAcceptToFormat(String accept) {
+        String format = "";
+        switch(accept) {
+            case "application/ld+json":
+                format = "JSON-LD";
+                break;
+            case "application/n-triples":
+                format = "N-Triples";
+                break;
+            case "application/rdf+xml":
+                format = "RDF/XML";
+                break;
+            case "application/turtle":
+                format = "TURTLE";
+                break;
+            case "application/rdf+json":
+                format = "RDF/JSON";
+                break;
+        }
+        return format;
+    }
+
+    public boolean isValidFormat(String accept) {
+        if (accept.equals("application/json") || accept.equals("application/ld+json") || accept.equals("application/n-triples") || accept.equals("application/rdf+xml") || accept.equals("application/turtle") || accept.equals("application/rdf+json") )
+            return true;
+
+        else
+            return false;
+    }
+
+    public String getCompanySparqlSelect(String companyID) {
+        String query = "PREFIX gr: <http://purl.org/goodrelations/v1#>\r\n" +
+                "PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>\r\n" +
+                "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n" +
+                "PREFIX exco: <http://localhost:8080/webservice/webapi/companies/>\r\n" +
+                "\r\n" +
+                "SELECT ?companyURL ?email ?catalogURI ?legalName\r\n" +
+                "\r\n" +
+                "WHERE{\r\n" +
+                "\r\n" +
+                "  exco:"+companyID+"	rdf:type		gr:BusinessEntity;\r\n" +
+                "                    vcard:hasURL	?companyURL;\r\n" +
+                "                    vcard:hasEmail	?email;\r\n" +
+                "       <http://schema.org/catalog>	?catalogURI;\r\n" +
+                "                    gr:legalName	?legalName	.\r\n" +
+                "  \r\n" +
+                "}\r\n" +
+                "";
+        return query;
+    }
+
+    public String getCompanySparqlDescribe(String companyID) {
+        String query = "PREFIX gr: <http://purl.org/goodrelations/v1#>\r\n" +
+                "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n" +
+                "PREFIX exco: <http://localhost:8080/webservice/webapi/companies/>\r\n" +
+                "\r\n" +
+                "DESCRIBE exco:" + companyID + "\r\n" +
+                "WHERE\r\n" +
+                "{\r\n" +
+                "exco:" + companyID + " rdf:type gr:BusinessEntity  .\r\n" +
+                "}\r\n" +
+                "";
+
+        return query;
+    }
     /*public List<Domain> getAllDomains(){
         List<Domain> domains = new ArrayList<>();
         Domain domain = new Domain();
